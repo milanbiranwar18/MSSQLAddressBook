7653357830fc68f8c27c7f4f52af2c04d7bde918
milanbiranwar18 <109934967+milanbiranwar18@users.noreply.github.com>
2022-09-02 20:57:04 +0530

    Query for editing Address book using person name
    Query for editing Address book using person name

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -1,2 +1,29 @@
 create database AddressBookService
 
+create table AddressBook
+(
+FirstName varchar(123),
+LastName varchar(123),
+Address varchar (200),
+City varchar (100),
+State varchar (100),
+Zip int,
+PhoneNumber varchar(123),
+Email varchar(123)
+)
+
+select * from AddressBook
+
+insert into AddressBook (FirstName,LastName, Address, City, State, Zip, PhoneNumber, Email)
+Values ('Milan', 'Biranwar', 'Katraj', 'Pune', 'Maharashtra', 441601, '8659965665', 'milanbiranwar@gmail.com'); 
+
+insert into AddressBook (FirstName,LastName,Address,City,State,Zip,PhoneNumber,Email)
+values ('Rajesh','Sharma','burdi','nagpur','Maharashtra',435601,'8698763235','rajeshsharma@gmail.com');
+
+insert into AddressBook (FirstName,LastName,Address,City,State,Zip,PhoneNumber,Email)
+values ('Ram','kawale','swargate','pune','Maharashtra',443601,'6998563235','ramkawale@gmail.com');
+
+insert into AddressBook (FirstName,LastName,Address,City,State,Zip,PhoneNumber,Email)
+values ('Suraj','Gupta','Bandra','Mumbai','Maharashtra',441201,'8698563235','surajgupta@gmail.com');
+
+update addressbook set PhoneNumber='6586987458' where FirstName='Milan'
