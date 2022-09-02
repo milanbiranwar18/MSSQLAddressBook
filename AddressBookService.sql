ba4fdad0d083ca0b845cff92a6e6fe4421dc1d4d
milanbiranwar18 <109934967+milanbiranwar18@users.noreply.github.com>
2022-09-02 20:30:33 +0530

    Query for creating Address Book Table
    Query for creating Address Book Table

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -1,2 +1,15 @@
 create database AddressBookService
 
+create table AddressBook
+(
+FirstName varchar(123),
+LastName varchar(123),
+Address varchar (200),
+City varchar (100),
+State varchar (100),
+Zip int,
+PhoneNumber int,
+Email varchar(123)
+)
+
+
